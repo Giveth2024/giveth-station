2bd93a84efdbe6ed4ad6a47d2e666f7fdea44a83
Giveth2024 <givendarian@gmail.com>
2025-12-13 17:01:48 +0300

    Added an alert to Welcome the user on the page.js file

--- app/page.js
+++ app/page.js
@@ -40,6 +40,7 @@ export default function LandingPage() {
 
   useEffect(() => {
     console.log("Welcome back to Giveth Station:)");
+    alert("Welcome back to Giveth Station:)");
     if (!serverRunning) checkServer();
     return () => clearTimeout(timeoutRef.current);
   }, [attempt, serverRunning]);
